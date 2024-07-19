0ff0a557e8fd283636d93ac634d10455d7a7476c
Lisii <lisii20041017@gmail.com>
2024-07-19 18:45:39 +0000

    merge multi launch

--- rmcs_flight/src/bringup/launch/launch.py
+++ rmcs_flight/src/bringup/launch/launch.py
@@ -3,18 +3,25 @@ from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution # type: ignore
+from launch_ros.actions import Node # type: ignore
+from launch_ros.substitutions import FindPackageShare # type: ignore
 
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    bringup_rmcs_flight = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(
-        		get_package_share_directory('rmcs_flight_bringup'),
-        		'launch/launch.py'
-        	)
-        )
+    params = [
+        PathJoinSubstitution([FindPackageShare("bringup"), "config", "flight_controller.yaml"])
+    ]
+
+    bringup_rmcs_flight = Node(
+        package="flight_controller",
+        executable="flight_controller_exe",
+        parameters=params,
+        output="screen",
+        respawn=True,
+        respawn_delay=1
     )
     ld.add_action(bringup_rmcs_flight)
 
